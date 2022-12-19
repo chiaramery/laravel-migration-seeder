28dbdc9d70c7d831adb402e89b175c153d6ecb6b
chiaramery <meringolachiara@gmail.com>
2022-12-19 17:42:28 +0100

    first commit

---
+++ resources/views/trains/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'All trains')
+
+@section('content')
+    <div class="container">
+        <h2>Lista dei treni</h2>
+        <ul>
+            @foreach ($trains as $train)
+            <li class="card col-5">
+            <p>Compagnia: {{$train->company}}</p> 
+            <p>Stazione di partenza: {{$train->departure_station}}, Data e orario: {{$train->departure_time}}</p> 
+            <p>Stazione di arrivo: {{$train->arrival_station}}, Data e orario: {{$train->arrival_time}}</p> 
+            <p>Codice treno: {{$train->code_train}}</p>
+            <p>Numero carrozze: {{$train->n_carriages}}</p>
+            </li>
+            
+                
+            @endforeach
+        </ul>
+    </div>
+@endsection
