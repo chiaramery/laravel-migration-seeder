1deaabf14de70529a0d19da1275fd210b61b1c6b
chiaramery <meringolachiara@gmail.com>
2022-12-20 13:01:56 +0100

    Miglioramenti

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $trains = Train::where('departure_time', '>=', now())->get();
         // dd($trains);
         return view('trains.index', compact('trains'));
     }
